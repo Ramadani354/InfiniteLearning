b717944447b8804b8c1c2f922e19324b8c3b36e4
Ramadani354 <yoviadhi123@gmail.com>
2024-05-13 19:46:05 +0700

    Update Divider di Home

--- app/src/main/java/com/tugas/android/screen/screen1.kt
+++ app/src/main/java/com/tugas/android/screen/screen1.kt
@@ -22,6 +22,7 @@ import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -112,6 +113,8 @@ fun screen1(navController: NavHostController) {
                     )
                 }
             }
+
+            Divider()
             LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp))
             {
                 items(ItemCount) { item ->
